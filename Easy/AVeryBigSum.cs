31dfb718c03ba015cd283752621ed5767fadfc03
Omar Samy <omar.samy3333@gmail.com>
2023-07-06 15:59:55 +0300

    Add AVeryBigSum easy problem

---
+++ Easy/AVeryBigSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_HackerRank_.Easy
+{
+	internal static class AVeryBigSum
+	{
+		public static long aVeryBigSum(List<long> ar)
+		{
+			return ar.Sum();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -9,4 +9,12 @@ Helper.PrintList(CompareTheTriplets.compareTriplets(new List<int>() { 17, 28, 30
 													 new List<int>() { 99, 16, 8 }
 													 )); //[2, 1]
 
+//A Very Big Sum
+Console.WriteLine(
+	AVeryBigSum.aVeryBigSum(new List<long>()
+	{ 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }
+							)); //5000000015
+
+
+
 
