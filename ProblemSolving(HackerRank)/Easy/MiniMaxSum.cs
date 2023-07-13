77e6c1382b68fc7289f587711a939f0604099174
Omar Samy <omar.samy3333@gmail.com>
2023-07-13 15:27:38 +0300

    Add a new easy problem - Mini-Max Sum, with its unit tests

---
+++ ProblemSolving(HackerRank)/Easy/MiniMaxSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_HackerRank_.Easy
+{
+	public static class MiniMaxSum
+	{
+		public static void miniMaxSum(List<int> arr)
+		{
+			arr.Sort();
+			var result = new long[2];
+			for (int i = 0; i < arr.Count - 1; i++)
+			{
+				result[0] += arr[i];
+				result[1] += arr[i + 1];
+			}
+
+			Console.WriteLine($"{result[0]} {result[1]}");
+        }
+	}
+}
--- ProblemSolving(HackerRank)/Program.cs
+++ ProblemSolving(HackerRank)/Program.cs
@@ -1,34 +1,42 @@
 ﻿using ProblemSolving_HackerRank_;
 using ProblemSolving_HackerRank_.Easy;
 
-//Simple Array Sum
-Console.WriteLine(SimpleArraySum.simpleArraySum(new List<int>() { 1, 2, 3, 4, 10, 11 })); //31
+MiniMaxSum.miniMaxSum(
+	   new List<int>() 
+	   { 256741038, 623958417, 467905213, 714532089, 938071625 }); //2063136757 2744467344
+	
+//TryAll();
+void TryAll()
+{
+	//Simple Array Sum
+	Console.WriteLine(SimpleArraySum.simpleArraySum(new List<int>() { 1, 2, 3, 4, 10, 11 })); //31
 
-//Compare the Triplets
-Helper.PrintList(CompareTheTriplets.compareTriplets(new List<int>() { 17, 28, 30 },
-													 new List<int>() { 99, 16, 8 }
-													 )); //[2, 1]
+	//Compare the Triplets
+	Helper.PrintList(CompareTheTriplets.compareTriplets(new List<int>() { 17, 28, 30 },
+														 new List<int>() { 99, 16, 8 }
+														 )); //[2, 1]
 
-//A Very Big Sum
-Console.WriteLine(
-	AVeryBigSum.aVeryBigSum(new List<long>()
-	{ 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }
-							)); //5000000015
+	//A Very Big Sum
+	Console.WriteLine(
+		AVeryBigSum.aVeryBigSum(new List<long>()
+		{ 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }
+								)); //5000000015
 
 
 
-var SquareMatrix = new List<List<int>>()
+	var SquareMatrix = new List<List<int>>()
 {
 	new List<int>(){1, 2, 3},
 	new List<int>(){4, 5, 6},
 	new List<int>(){9, 8, 9},
 };
 
-Helper.PrintSquareMatrix(SquareMatrix);
-Console.WriteLine(DiagonalDifference.diagonalDifference(SquareMatrix)); //2
+	Helper.PrintSquareMatrix(SquareMatrix);
+	Console.WriteLine(DiagonalDifference.diagonalDifference(SquareMatrix)); //2
 
 
-PlusMinus.plusMinus(new List<int>() { -4, 3, -9, 0, 4, 1 });
+	PlusMinus.plusMinus(new List<int>() { -4, 3, -9, 0, 4, 1 });
 
 
-Staircase.staircase(6);
+	Staircase.staircase(10);
+}
