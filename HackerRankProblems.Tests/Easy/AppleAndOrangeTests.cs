43332e179c0fa033e3d67c5e64ffa99a6a190d6e
Elmohammady <A0093700@kiongroup.com>
2024-10-20 11:11:35 +0300

    Add a new easy problem, Apple And Orange Problem, with unit test.

---
+++ HackerRankProblems.Tests/Easy/AppleAndOrangeTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using ProblemSolving_HackerRank_.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems.Tests.Easy
+{
+    public class AppleAndOrangeTests
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { 7, 10, 4, 12, new List<int>() { 2, 3, -4 }, new List<int>() { 3, -2, -4 }, "1" };
+            yield return new object[] { 7, 10, 4, 12, new List<int>() { 2, 3, -4 }, new List<int>() { 3, -2, -4 }, "2" };
+
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void AppleAndOrange_countApplesAndOranges_WriteApplesCoundAndOrangeCountThatFallOnHouse(int s, int t, int a, int b, List<int> apples, List<int> oranges, string expectedResult)
+        {
+            //Redirect console output to capture it
+            using (StringWriter sw = new StringWriter())
+            {
+                //Arrange
+                Console.SetOut(sw);
+
+                //Act
+                AppleAndOrange.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+                //Get the captured console output
+                string consoleOutput = sw.ToString().Trim();
+
+                //Assert
+                consoleOutput.Contains(expectedResult);
+
+            }
+        }
+    }
+}
---
+++ ProblemSolving(HackerRank)/Easy/AppleAndOrange.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_HackerRank_.Easy
+{
+    public static class AppleAndOrange
+    {
+        /// <summary>
+        /// Sam's house has an apple tree and an orange tree that yield an abundance of fruit. 
+        /// Using the information given below, determine the number of apples and oranges that land on Sam's house.
+        /// Complete the 'countApplesAndOranges' function below.
+        /// </summary>
+        /// <param name="s">House start point</param>
+        /// <param name="t">House end point</param>
+        /// <param name="a">Apple tree position point</param>
+        /// <param name="b">Orange tree position point</param>
+        /// <param name="apples">Thrown apples points</param>
+        /// <param name="oranges">Thrown oranges points</param>
+
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            Console.WriteLine(GetCatchedFruit(apples, a, s, t));
+            Console.WriteLine(GetCatchedFruit(oranges, b, s, t));
+        }
+
+        private static int GetCatchedFruit(List<int> fruit, int treePos, int startPoint, int endPoint)
+        {
+            int Catchedfruit = 0;
+
+            for (int i = 0; i < fruit.Count; i++)
+            {
+                int AppleAbsPos = fruit[i] + treePos;
+                if (AppleAbsPos >= startPoint && AppleAbsPos <= endPoint) Catchedfruit++;
+            }
+
+            return Catchedfruit;
+        }
+    }
+}
